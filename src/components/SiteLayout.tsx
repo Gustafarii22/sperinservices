c5351482ef1f517fc4ee9530b40e45e2fa2f9c38
Gustafarii22 <gussysperin@yahoo.co.uk>
2026-09-23 15:55:28 +0100

    Add guided quick quote assistant

--- src/components/SiteLayout.tsx
+++ src/components/SiteLayout.tsx
@@ -2,6 +2,7 @@ import { Header } from "./Header";
 import { Footer } from "./Footer";
 import { MobileBar } from "./MobileBar";
 import { WhatsAppButton } from "./WhatsAppButton";
+import { QuoteAssistant } from "./QuoteAssistant";
 
 export function SiteLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -10,6 +11,7 @@ export function SiteLayout({ children }: { children: React.ReactNode }) {
       <main className="flex-1 pb-24 md:pb-0">{children}</main>
       <Footer />
       <MobileBar />
+      <QuoteAssistant />
       <div className="hidden md:block fixed bottom-5 right-5 z-40">
         <WhatsAppButton iconOnly />
       </div>
